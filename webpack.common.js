2b9719af212426d2ccd15011d3ccd89f025d61cb
Satya Wikananda <satyawikananda456@gmail.com>
2020-06-15 22:02:14 +0800

    :sparkles: Add linter and prettier

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,34 @@
-const path = require("path")
-const HTMLWebpackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: "./src/index.html",
-            filename: "index.html"
-        })
-    ]
-}
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
